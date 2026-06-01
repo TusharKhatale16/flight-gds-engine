e7a8a9ca52455dca81bfa23a1e01bb5f26a9f5ef
TusharKhatale16 <tusharkhatale16@gmail.com>
2026-06-01 16:56:10 +0530

    Connection added in resource block

--- src/main/java/com/airline/resource/Database.java
+++ src/main/java/com/airline/resource/Database.java
@@ -1,5 +1,20 @@
 package com.airline.resource;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
 
+	public static Connection createConnection() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql//localhost:3306/flight_gds_db", "root","Tushar@16");
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return con;	
+	}
 }
